7e0d04ebfa9f3ea5ba7a6125e1643e252391d8c1
Tulio Minini <tulio.mminini@gmail.com>
2024-07-21 13:24:48 +0100

    define mobile width and height, add id to html elements.on javascript, target html elements by id and create sounds placeholders

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,2 +1,24 @@
-let test = 'test';
-console.log(test);
+// home screen elements
+let playBtn = document.getElementById('play-btn');
+let rulesBtn = document.getElementById('rules-btn');
+
+// game screen elements
+let topGameScreen = document.getElementById('top-screen-text');
+let levelScreen = document.getElementById('level-screen-number');
+let greenBtn = document.getElementById('green-btn');
+let redBtn = document.getElementById('red-btn');
+let yellowBtn = document.getElementById('yellow-btn');
+let blueBtn = document.getElementById('blue-btn');
+let backBtn = document.getElementById('back-btn');
+let startBtn = document.getElementById('start-btn');
+let resetBtn = document.getElementById('reset-btn');
+
+// game sounds
+let greenSound = new Audio('');
+let redSound = new Audio('');
+let yellowSound = new Audio('');
+let blueSound = new Audio('');
+let correctSound = new Audio('');
+let wrongSound = new Audio('');
+
+// functions
